71ff361af706f29bba7d9ce4ef638f92f8942fa1
Himanshu Gupta <himanshugupta558@gmail.com>
2024-07-29 02:22:42 +0530

    added code for palindrome assignment

---
+++ Fullstack_Development/Assignments/01-js/medium/palindrome.js
@@ -0,0 +1,15 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  str = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+  if(str.toLowerCase() === str.toLowerCase().split('').reverse().join(''))
+    return true;
+  return false;
+}
+
+console.log(isPalindrome("Hannah"));
+
+module.exports = isPalindrome;
